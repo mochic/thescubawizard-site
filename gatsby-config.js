fa16b0f1f59127e209fd6e8b1fc2d010cafd48ae
Christopher Mochizuki <mochic808@gmail.com>
2020-02-09 20:45:15 -0800

    try progressive web app stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,6 +66,6 @@ module.exports = {
     `gatsby-plugin-remove-console`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
